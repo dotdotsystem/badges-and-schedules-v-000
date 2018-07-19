ff6021d4e242a83d7f87274149cbaa54e550bf1c
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-19 01:00:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  names = Array.new
+  name.each { |names|}
+  puts "Hello, 
+end
+
+def assign_rooms(name)
+assignment = Array.new
+name.each_with_index |room|
+puts "Hello, #{name}! You'll be assigned to #{room}!"
+end
