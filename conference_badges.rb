a5e7ffd0982b12ee1f52a1d3f7656d85873c9b1e
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-19 01:18:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,9 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array).each {|id|
-    puts id }
+  batch_badge_creator(array).each do |id|
+    puts id 
+  end
 assign_rooms(array).each do |id|
   puts id
 end
